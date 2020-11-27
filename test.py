ade895c9f62f4f25756c4a0c73ada6ea43e7a7c1
Ada Xiang <adaxiang@Adas-MacBook-Pro.local>
2020-11-27 16:05:24 +0800

    finished filing doc tranche level data extraction

--- test.py
+++ test.py
@@ -1,11 +1,32 @@
 from scrapers.KRXscraper import scraper as krx_scraper
 from scrapers.DARTscraper import scraper as dart_scraper
 
-if __name__ == "__main__":
-    # krxscraper = krx_scraper()
-    # result_df = krxscraper._search_by_date(startdate='20190101', enddate='20200101')
-    # print(result_df)
+import argparse
 
+def test_krxscraper():
+    krxscraper = krx_scraper()
+    result_df = krxscraper._search_by_date(startdate='20190101', enddate='20200101')
+    print(result_df)
+
+
+def test_get_filing_doc():
+    dartsraper = dart_scraper()
+    dartsraper.search(company_name="빅히트엔터테인먼트", start_date="20200524", end_date="20201124")
+    is_found = dartsraper.find_doc(doc_name='filing_doc')
+
+    dartsraper.get_filing_doc_info()
+    print(f'''Found the doc {is_found}\n
+              annoucement_date: {dartsraper.annouce_dates}\n
+              new_shares: {dartsraper.new_shares}\n
+              second_shares: {dartsraper.second_shares}\n
+              share outstandings: {dartsraper.share_outstandings}\n
+              tranche filing high range {dartsraper.tranche_filing_high}\n
+              tranche filing low range {dartsraper.tranche_filing_low}''')
+
+    dartsraper.driver.quit()
+
+
+def test_get_offer_doc():
     dartsraper = dart_scraper()
     dartsraper.search(company_name="빅히트엔터테인먼트", start_date="20200524", end_date="20201124")
     is_found = dartsraper.find_doc(doc_name='offer_doc')
@@ -15,14 +36,30 @@ if __name__ == "__main__":
               subsprition start date: {dartsraper.sub_startdates}\n
               subsprition end date: {dartsraper.sub_enddates}\n
               settlement date: {dartsraper.settle_dates}\n
-              use of proceeds: {dartsraper.uops}
-              ''')
+              use of proceeds: {dartsraper.uops}\n''')
+
+    dartsraper.driver.quit()
+
+
+if __name__ == "__main__":
+    test_map = {
+        'krxscraper':test_krxscraper, 
+        'get_filing_doc':test_get_filing_doc, 
+        'get_offer_doc':test_get_offer_doc
+    }
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-test', type=str, required=True, help=f'choose an action from {test_map.keys()}')
+
+    args = arg_parser.parse_args()
+    if args.test not in test_map.keys():
+        raise ValueError("No such test available")
+    else:
+        test_map[args.test]()
 
     
-    # dartsraper.get_filing_doc_info()
-    # print(f'''Found the doc {is_found}\n
-    #           annoucement_date: {dartsraper.annouce_dates}\n
-    #           new_shares: {dartsraper.new_shares}\n
-    #           second_shares: {dartsraper.second_shares}\n
-    #           share outstandings {dartsraper.share_outstandings}\n''')
-    dartsraper.driver.quit()
+
+    
+
+    
+    
+    
